50337a559c8c7573e9dd6395869dbe7d50af0c54
Juan Luis Rodriguez <mamanirodriguez@gmail.com>
2026-07-28 12:38:42 +0100

    Fix variable names

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let list = [
+    let gifts = [
         "Twelve drummers drumming",
         "Eleven pipers piping",
         "Ten lords a-leaping",
@@ -9,11 +9,11 @@ fn main() {
         "Six geese a-laying",
         "Five golden rings",
         "Four calling birds",
-        "Three french hens",
+        "Three French hens",
         "Two turtle doves and",
         "A partridge in a pear tree"
         ];
-    let ordinal = [
+    let ordinals = [
         "first", 
         "second", 
         "third", 
@@ -29,10 +29,10 @@ fn main() {
 
     for day in 1..=12 {
         println!("\nVerse [{}]", day);
-        println!("On the {} day of Chirstmas my true love sent to me", ordinal[day-1]);
+        println!("On the {} day of Christmas my true love sent to me", ordinals[day-1]);
 
         for daily_gift in (1..=day).rev() {
-            println!("{}",list[12-daily_gift])
+            println!("{}", gifts[12-daily_gift]);
         }
     }
 }
